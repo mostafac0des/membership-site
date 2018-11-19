739e2e0adcd57d50c0b9d20544a8471917cf79ef
Mostafa Abdelbaky <>
2018-11-19 12:06:54 -0800

    add ssl capability

--- ubuntu-compose.sh
+++ ubuntu-compose.sh
@@ -4,8 +4,14 @@
 # install latest version of docker the lazy way
 curl -sSL https://get.docker.com | sh
 
+# add lets encrypt
+sudo add-apt-repository ppa:certbot/certbot
+
 # upgrade packages
 sudo apt update
+
+# install certbot
+sudo apt-get install python-certbot-nginx
 
 # make it so you don't need to sudo to run docker commands
 usermod -aG docker ubuntu
@@ -13,3 +19,9 @@ usermod -aG docker ubuntu
 # install docker and docker-compose
 sudo apt install docker-compose
 sudo apt install docker-ce
+
+# open firewall
+sudo ufw enable
+sudo ufw allow 80
+sudo ufw allow 443
+sudo ufw allow 'OpenSSH'
