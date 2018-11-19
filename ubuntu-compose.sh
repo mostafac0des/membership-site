2052091b0f2cc5f01a481bc0cd925c266bf110e4
Mostafa Abdelbaky <>
2018-11-19 10:12:59 -0800

    script file update

--- ubuntu-compose.sh
+++ ubuntu-compose.sh
@@ -1,14 +1,15 @@
 
 #!/bin/bash
 
+# install latest version of docker the lazy way
+curl -sSL https://get.docker.com | sh
+
+# upgrade packages
 sudo apt update
 
 # make it so you don't need to sudo to run docker commands
 usermod -aG docker ubuntu
 
-# install docker-compose
+# install docker and docker-compose
 sudo apt install docker-compose
-
-# install docker
 sudo apt install docker-ce
-sudo chmod +x docker-compose
