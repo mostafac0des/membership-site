fbc6309d55c3266d93718984f5be91dee82816d4
Mostafa Abdelbaky <>
2018-11-25 11:29:38 -0800

    update deploy tasks

--- site/deploy-tasks.sh
+++ site/deploy-tasks.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Build images
+echo "Bulding images.. "
+docker-compose build
+
 # Apply database migrations
 echo "Apply database migrations"
 docker-compose run web ./manage.py migrate
