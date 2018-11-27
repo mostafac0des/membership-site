bbc21f367321ec89756c0237ab284a2cac509aee
Mostafa Abdelbaky <>
2018-11-27 10:55:47 -0800

    update migrations files

--- site/apps/users/models.py
+++ site/apps/users/models.py
@@ -1,10 +1,7 @@
 from django.contrib.auth.models import AbstractUser
-from django.contrib.sites.models import Site
-from django.db import models
 
 
 class CustomUser(AbstractUser):
-    site = models.ForeignKey(Site, on_delete=models.CASCADE)
     pass
 
     def __str__(self):
